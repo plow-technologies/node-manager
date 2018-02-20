31e06f0e2d6d29921379a0d17b74f52b60a6e56f
Brent <brent80@users.noreply.github.com>
2018-02-20 15:38:58 -0600

    Delete preOutgoing.sh

--- preOutgoing.sh
+++
@@ -1,5 +0,0 @@
-cp ./dist/build/node-manager/node-manager ./
-tar -czvf node-manager.tar.gz node-manager
-rsync -avzhe ssh --progress node-manager.tar.gz node@alarm.plowtech.net:/home/node/incoming/
-rm ./node-manager
-rm ./node-manager.tar.gz
